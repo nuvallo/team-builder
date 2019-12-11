843df89dbb36ad9c5f7c0fad170c7a8635025b6a
Tyler <nuvallo.inc@gmail.com>
2019-12-11 15:54:53 -0500

    setting up file structure

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
-function App() {
-  return null;
-}
+import App from "../src/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
